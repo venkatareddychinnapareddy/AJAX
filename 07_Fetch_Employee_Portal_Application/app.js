7e66e3131986febec7730f6c7f905370872a2682
venkata reddy <vasuvenkat9396@gmail.com>
2021-03-28 11:40:59 +0530

    finishes

--- 07_Fetch_Employee_Portal_Application/app.js
+++ 07_Fetch_Employee_Portal_Application/app.js
@@ -20,10 +20,10 @@ let fetchEmployees = () => {
                                     <td>${employee.email}</td>
                                     <td>${employee.gender}</td>
                                     <td>${employee.ip_address}</td>
-                                     <td>
+                                    <td>
                                         <button class="btn btn-secondary mt-0 btn-sm update form-inline-group">Update</button>
                                         <button class="btn btn-danger mt-0 btn-sm delete form-inline-group">Delete</button>                                 
-                                </td>
+                                    </td>
                               </tr>`;
         }
         document.querySelector('#table-body').innerHTML = employeeRows;
@@ -62,4 +62,69 @@ let clearFormFields = () => {
          document.querySelector('#add-email').value = '';
          document.querySelector('#add-gender').value = '';
          document.querySelector('#add-ip-address').value = '';
-}
+};
+
+
+//click on table body 
+let tableBody = document.querySelector('#table-body');
+tableBody.addEventListener('click', (e) => {
+    let targetElement = e.target;
+    //Delete button
+    if(targetElement.classList.contains('delete')){
+        let selectedID = targetElement.parentElement.parentElement.firstElementChild.innerHTML;
+        let url = `${serverURL}/employees/${selectedID}`;
+        BrainHttp.delete(url).then((data) => {
+            console.log(data);
+            fetchEmployees();
+        }).catch((error) => {
+            console.log(error);
+        });
+
+    }
+    // Update button
+    if(targetElement.classList.contains('update')){
+       let selectedID = targetElement.parentElement.parentElement.firstElementChild.innerHTML;
+       let url = `${serverURL}/employees`;
+       BrainHttp.get(url).then((data) => {
+            let employees = data;
+            let selectedEmployee = employees.find((employee) => {
+                return employee.id  === selectedID.trim();
+            });
+            populateUpdateEmployeeModal(selectedEmployee);
+       }).catch((error) => {
+        console.error(error);
+       });
+    }
+});
+
+let populateUpdateEmployeeModal = (selectedEmployee) => {
+        document.querySelector('#update-emp-id').value = selectedEmployee.id;
+        document.querySelector('#update-first-name').value = selectedEmployee.first_name;
+        document.querySelector('#update-last-name').value = selectedEmployee.last_name;
+        document.querySelector('#update-email').value = selectedEmployee.email;
+        document.querySelector('#update-gender').value = selectedEmployee.gender;
+        document.querySelector('#update-ip-address').value = selectedEmployee.ip_address;
+        $('#update-employee').modal('show'); //to show the modal
+};
+
+//Update employee form
+let updateEmployeeForm = document.querySelector('#update-employee-form');
+updateEmployeeForm.addEventListener('submit' , () => {
+
+        let updateEmployeeID = document.querySelector('#update-emp-id').value.trim();
+        let url = `${serverURL}/employees/${updateEmployeeID}`;
+        $('#update-employee').modal('hide'); //to close the modal
+        let employee = {
+            first_name : document.querySelector('#update-first-name').value,
+            last_name : document.querySelector('#update-last-name').value,
+            email : document.querySelector('#update-email').value,
+            gender : document.querySelector('#update-gender').value,
+            ip_address : document.querySelector('#update-ip-address').value,
+        };
+        BrainHttp.put(url , employee).then((data) => {
+            console.log(data);
+            fetchEmployees();
+        }).catch((err) => {
+            console.error(err);
+        });
+    }); 
